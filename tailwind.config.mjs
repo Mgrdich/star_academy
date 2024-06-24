eedf34d3f32e2e151eaab0c52b40b4a037a47d96
Mgrdich <mgotm13@gmail.com>
2024-06-25 00:55:15 +0400

    Create Header + add alpine integrations + fix the tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,24 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      black: "#08111F",
+      primary: "#4F46E5",
+      secondary: "#ea47ed",
+    },
     extend: {},
   },
   plugins: [],
