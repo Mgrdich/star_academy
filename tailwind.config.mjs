2857e9f76729c0b391f7c964301393c680d3a775
Mgrdich <46796009+Mgrdich@users.noreply.github.com>
2024-06-26 00:53:55 +0400

    Chore/Home and About Us Components (#1)
    * Create Header + add alpine integrations + fix the tailwind config
    
    * Sections Creation + Code fixes
    
    * Add Home and AboutUs sections
    
    * Fix Tailwind config and Blockquote styling fixes
    
    * Styling Fixes for the components , Linting fixes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,27 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        white: "#ffffff",
+        black: "#08111F",
+        primary: "#A8CBEB",
+        secondary: "#C05D59",
+        tertiary: "#FEF4C7",
+      },
+    },
   },
   plugins: [],
 };
