ae3e2616a671fda95aee464fb694cd27b058ae74
Mgrdich <mgotm13@gmail.com>
2024-06-26 03:15:00 +0400

    Fix linter issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs(), icon()]
+  integrations: [tailwind(), alpinejs(), icon()],
 });
