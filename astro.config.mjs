910cce8b0908f1ec3f22d2a03f62f88fc7f51e66
Mgrdich <46796009+Mgrdich@users.noreply.github.com>
2024-06-29 00:47:48 +0400

    Fix the scrolling , remove alpine plugin (#11)
    * Fix the scrolling , remove alpine plugin
    
    * Fix the build issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import alpinejs from "@astrojs/alpinejs";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs(), icon()],
+  integrations: [tailwind(), icon()],
   site: "https://staracademymontessori.ca",
 });
